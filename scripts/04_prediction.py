4808370992e683cfde337cbed86a06f22f46901d
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-21 23:57:25 -0800

    04 script complete

--- scripts/04_prediction.py
+++ scripts/04_prediction.py
@@ -1,3 +1,16 @@
-# Load trained model and cleaned dataset
-# Take sample inputs and predict admission chances
-# Output predictions
+import pandas as pd
+import joblib
+
+def predict(input_features, model_path):
+    model = joblib.load(model_path)
+    df = pd.DataFrame([input_features])
+    prediction = model.predict(df)
+    return prediction[0]
+
+if __name__ == "__main__":
+    sample_input = {
+        "GRE_Score": 0.9, "TOEFL_Score": 0.85, "University_Rating": 4,
+        "SOP": 4.5, "LOR": 4.5, "CGPA": 0.95, "Research": 1
+    }
+    pred = predict(sample_input, "../outputs/admission_model.pkl")
+    print(f"Predicted Admission Chance: {pred:.3f}")
